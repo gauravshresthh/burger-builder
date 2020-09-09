db35f5431432b6dfbdbd1551339fbd396e887113
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-09-09 22:24:51 +0545

    BurgerButton added to dynamically add burger contents

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,50 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './BurgerIngredient.scss';
+const BurgerIngredient = (props) => {
+  let ingredient = null;
 
-const BurgerIngredient = (this.props.) => {
-    let ingredient=null;
+  switch (props.type) {
+    case 'bread-bottom':
+      ingredient = <div className="BreadBottom"></div>;
+      break;
 
-switch(props.type){
-    case ('bread-bottom'):
-        ingredient=<div className='BreadBottom'></div>;
-    break;
+    case 'bread-top':
+      ingredient = (
+        <div className="BreadTop">
+          <div className="Seed1"></div>
+          <div className="Seed2"></div>
+        </div>
+      );
+      break;
 
-    case('bread-top'):
-    ingredient=(<div className="BreadTop">
-        <div className="Seed1"></div> 
-        <div className="Seed2"></div>
-    </div>
-    );
-    break;
+    case 'meat':
+      ingredient = <div className="Meat"></div>;
+      break;
 
-    case('meat'):
-    ingredient=<div className="Meat"></div>
-    break;
+    case 'cheese':
+      ingredient = <div className="Cheese"></div>;
+      break;
 
-    case('cheese'):
-    ingredient=<div className="Cheese"></div>
-    break;
+    case 'bacon':
+      ingredient = <div className="Bacon"></div>;
+      break;
 
-    case('bacon'):
-    ingredient=<div className="Bacon"></div>
-    break;
-
-    case('salad'):
-    ingredient=<div className="Salad"></div>
-    break;
+    case 'salad':
+      ingredient = <div className="Salad"></div>;
+      break;
 
     default:
-        ingredient=null;
-  
-}
-return ingredient;
+      ingredient = null;
+  }
+  return ingredient;
+};
 
-}
+BurgerIngredient.PropTypes = {
+  type: PropTypes.string.isRequired,
+};
 
-BurgerIngredient.PropTypes={
-    type : PropTypes.string.isRequired
-
-}
-
-
-export default BurgerIngredient
+export default BurgerIngredient;
